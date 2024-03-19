8f3a2b0ee542c2674556325be445e3728028ba9b
root <whqkrqkr12345@gmail.com>
2024-03-19 23:30:20 +0900

    Initial Commit

---
+++ SafeCheck/SafeCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeCheckApp.swift
+//  SafeCheck
+//
+//  Created by 조근호 on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
