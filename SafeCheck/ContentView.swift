9e85843c1461a0c3c27b26cb90c84c5478092817
root <whqkrqkr12345@gmail.com>
2024-03-23 14:56:39 +0900

    ADD Login Button

--- SafeCheck/ContentView.swift
+++ SafeCheck/ContentView.swift
@@ -2,25 +2,34 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var isPresent: Bool = false
     @State var isPresented: Bool = false
     @State var image: UIImage?
     
     var body: some View {
-        VStack {
-            if let image {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-            }
-            
-            Button {
-                isPresented.toggle()
-            } label: {
-                Text("버튼")
-            }
+        Button{
+            isPresent.toggle()
+        } label: {
+            Text("로그인")
         }
-        .fullScreenCover(isPresented: $isPresented) {
-            CameraView($image)
+        .fullScreenCover(isPresented: $isPresent) {
+            VStack {
+                if let image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                Button {
+                    isPresented.toggle()
+                } label: {
+                    Text("버튼")
+                }
+            }
+            .fullScreenCover(isPresented: $isPresented) {
+                CameraView($image)
+            }
         }
     }
 }
+
