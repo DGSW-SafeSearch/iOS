262a590008dd5dadd015f18c2d4646fa14151b67
root <whqkrqkr12345@gmail.com>
2024-03-25 14:14:07 +0900

    Updated login

--- SafeCheck/Login/GoogleView.swift
+++ SafeCheck/Login/GoogleView.swift
@@ -4,13 +4,28 @@ import GoogleSignIn
 import GoogleSignInSwift
 
 struct GoogleView: View {
+    @State var google: GoogleModel?
+    
     var body: some View {
         Text("A")
     }
     
     func login() {
-        
+        AF.request("http://10.80.163.32:8082/auth/google")
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let responseData = try JSONDecoder().decode(google.self, from: data)
+                        self.google = responseData
+                    } catch {
+                        print(error)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
+    
 }
-
-
