55f9980d758d1eee3b3df2cc7995990535c415f6
root <whqkrqkr12345@gmail.com>
2024-03-26 18:08:06 +0900

    Updated Goggle

--- SafeCheck/Google/Google.swift
+++ SafeCheck/Google/Google.swift
@@ -4,7 +4,7 @@ import GoogleSignInSwift
 
 class Google: ObservableObject {
     @Published var googleLogin: Login?
-    @Published var idToken: String = ""
+    @Published var dmdi: String = ""
     
     func handleSignInButton() {
         var rootViewController: UIViewController {
@@ -18,41 +18,41 @@ class Google: ObservableObject {
         
         GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController ) { signInResult, error in
             guard let signInResult = signInResult else { return }
-            
             let user = signInResult.user
-            let idToken = user.idToken
-            print(idToken!)
-      
+            self.dmdi = user.profile?.email ?? ""
+            print(self.dmdi)
+            self.login()
         }
     }
     
     func login() {
         
         let query : Parameters = [
-            "idToken" : idToken
+            "emailAddress" : dmdi
         ]
         
         AF.request("http://10.80.163.106:8082/auth/google",
-                    method: .post,
-                    parameters: query,
-                    encoding: JSONEncoding.default)
-            .responseData { response in
-                switch response.result {
-                case .success(let data):
-                    do {
-                        let responseData = try JSONDecoder().decode(Login.self, from: data)
-                        self.googleLogin = responseData
-                    } catch {
-                        print(error)
-                    }
-                case .failure(let error):
+                   method: .post,
+                   parameters: query,
+                   encoding: JSONEncoding.default)
+        
+        .responseData { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    let responseData = try JSONDecoder().decode(Login.self, from: data)
+                    self.googleLogin = responseData
+                } catch {
                     print(error)
                 }
+            case .failure(let error):
+                print(error)
             }
-                
+        }
+        
     }
 }
 
 struct Login: Codable {
-    let login: [String]
+    let newUser: String
 }
