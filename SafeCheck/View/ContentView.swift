f06ee57382196014d1e6242c0bc0c0ee670cc29b
root <whqkrqkr12345@gmail.com>
2024-04-09 20:03:46 +0900

    Created MainView

--- SafeCheck/View/ContentView.swift
+++ SafeCheck/View/ContentView.swift
@@ -3,10 +3,7 @@ import GoogleSignIn
 import GoogleSignInSwift
 
 struct ContentView: View {
-    @ObservedObject private var vision = Vision()
     @ObservedObject private var google = Google()
-    @State var isPresented: Bool = false
-    @State var image: UIImage?
     
     var body: some View {
         VStack {
@@ -14,40 +11,8 @@ struct ContentView: View {
                 google.handleSignInButton()
             }
             .padding()
-            
-            Button("A") {
-                google.isPresent.toggle()
-            }
             .fullScreenCover(isPresented: $google.isPresent) {
-                if let image = image {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .onAppear {
-                            vision.reText(image: image)
-                        }
-                }
-                
-                Text(vision.ocrString ?? "__empty__")
-                Text(vision.casNumber ?? "__empty__")
-                Text(vision.unNumber ?? "__empty__")
-                
-                HStack {
-                    Button("사진") {
-                        isPresented.toggle()
-                    }
-                    .fullScreenCover(isPresented: $isPresented) {
-                        CameraView($image)
-                    }
-                    .onChange(of: image) { _ in
-                        if let image = image {
-                            vision.reText(image: image)
-                        }
-                    }
-                    Button("으야") {
-                        vision.informationed()
-                    }
-                }
+                MainView()
             }
         }
     }
--- SafeCheck/View/MainView.swift
+++ SafeCheck/View/MainView.swift
@@ -1,8 +1,39 @@
-//
-//  MainView.swift
-//  SafeCheck
-//
-//  Created by 조근호 on 4/9/24.
-//
+import SwiftUI
 
-import Foundation
+struct MainView: View {
+    @ObservedObject private var vision = Vision()
+    @State var isPresented: Bool = false
+    @State var image: UIImage?
+    
+    var body: some View {
+        if let image = image {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .onAppear {
+                    vision.reText(image: image)
+                }
+        }
+        
+        Text(vision.ocrString ?? "__empty__")
+        Text(vision.casNumber ?? "__empty__")
+        Text(vision.unNumber ?? "__empty__")
+        
+        HStack {
+            Button("사진") {
+                isPresented.toggle()
+            }
+            .fullScreenCover(isPresented: $isPresented) {
+                CameraView($image)
+            }
+            .onChange(of: image) { _ in
+                if let image = image {
+                    vision.reText(image: image)
+                }
+            }
+            Button("으야") {
+                vision.informationed()
+            }
+        }
+    }
+}
