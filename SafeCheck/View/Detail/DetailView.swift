1b68edaeaccdaad633a23a99bca4c485f47303ef
root <whqkrqkr12345@gmail.com>
2024-09-30 13:42:33 +0900

    Updated DetailView

--- SafeCheck/View/Detail/DetailView.swift
+++ SafeCheck/View/Detail/DetailView.swift
@@ -11,38 +11,37 @@ struct DetailView: View {
                 .ignoresSafeArea()
             
             // MARK: - 뷰
+            VStack {
+                ScrollView {
+                    Image("main")
+                        .padding(30)
+                    Spacer()
+                    if let cas_number = vision.information?.chemical_substance {
+                        ForEach(cas_number, id: \.self) { row in
+                            VStack(spacing: 0) {
+                                InfoRow(title: "한국어 이름", value: row.korean_name ?? "__empty__")
+                                InfoRow(title: "영어 이름", value: row.english_name ?? "__empty__")
+                                InfoRow(title: "CAS 번호", value: row.cas_number ?? "__empty__")
+                                InfoRow(title: "UN 번호", value: row.un_number ?? "__empty__")
+                                InfoRow(title: "기존 코드", value: String(row.chemical_id?.description ?? "__empty__"))
+                            }
+                            .padding(.horizontal, 15)
+                            .padding(.bottom)
+                        }
+                    }
+                    Spacer()
+                }
+            }
+            
             GeometryReader { geometry in
                 Button(action: {
                     vision.casNumber.removeAll()
                     dismiss()
                 }) {
                     Image("back")
-                        .position(x: geometry.size.width * 0.05, y: geometry.size.height * 0.05)
+                        .padding()
                 }
-                
-                VStack {
-                ScrollView {
-                    Image("main")
-                        .padding(30)
-                    Spacer()
-                        if let cas_number = vision.information?.chemical_substance {
-                            ForEach(cas_number, id: \.self) { row in
-                                VStack(spacing: 0) {
-                                    InfoRow(title: "한국어 이름", value: row.korean_name ?? "__empty__")
-                                    InfoRow(title: "영어 이름", value: row.english_name ?? "__empty__")
-                                    InfoRow(title: "CAS 번호", value: row.cas_number ?? "__empty__")
-                                    InfoRow(title: "UN 번호", value: row.un_number ?? "__empty__")
-                                    InfoRow(title: "기존 코드", value: String(row.chemical_id?.description ?? "__empty__"))
-                                }
-                                .padding(.horizontal, 15)
-                                .padding()
-                            }
-                        }
-                    Spacer()
-                        Image("footer")
-                            .padding()
-                    }
-                }
+                .position(x: geometry.size.width * 0.06, y: geometry.size.height * 0.05)
             }
         }
     }
